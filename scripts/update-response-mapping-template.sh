282a168287d723de0aa307dafebc24bca9ec336c
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-07-29 19:43:02 +0530

    removed bash package

--- scripts/update-response-mapping-template.sh
+++ scripts/update-response-mapping-template.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-# Load environment variables from GitHub Actions secrets or CodeBuild environment variables
-API_ID=$API_ID
-
-# Extract queries and mutations from the Appsyncupdate.yaml file
-QUERY_LIST=$(yq eval '.queries[]' src/Appsyncupdate.yml)
-MUTATION_LIST=$(yq eval '.mutations[]' src/Appsyncupdate.yml)
+# Read the list of queries from the file
+QUERY_LIST=$(cat queries.txt)
 
 # Update response mapping templates for Queries
 for query in $QUERY_LIST; do
-  aws appsync update-resolver \
-    --api-id $API_ID \
-    --type-name Query \
-    --field-name $query \
-    --response-mapping-template file://src/Queries/$query/response_mapping_template.graphql
+  if [[ -f src/Queries/$query/response_mapping_template.graphql ]]; then
+    aws appsync update-resolver \
+      --api-id $API_ID \
+      --type-name Query \
+      --field-name $query \
+      --response-mapping-template file://src/Queries/$query/response_mapping_template.graphql \
+      --data-source-name LambdaDataSource
+  fi
 done
+
+# Read the list of mutations from the file
+MUTATION_LIST=$(cat mutations.txt)
 
 # Update response mapping templates for Mutations
 for mutation in $MUTATION_LIST; do
-  aws appsync update-resolver \
-    --api-id $API_ID \
-    --type-name Mutation \
-    --field-name $mutation \
-    --response-mapping-template file://src/Mutations/$mutation/response_mapping_template.graphql
+  if [[ -f src/Mutations/$mutation/response_mapping_template.graphql ]]; then
+    aws appsync update-resolver \
+      --api-id $API_ID \
+      --type-name Mutation \
+      --field-name $mutation \
+      --response-mapping-template file://src/Mutations/$mutation/response_mapping_template.graphql \
+      --data-source-name LambdaDataSource
+  fi
 done
