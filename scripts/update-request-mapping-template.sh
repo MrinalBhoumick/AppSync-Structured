43c652e1cd3106fdfede11568355c5cef1dbfad6
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-07-29 19:53:59 +0530

    removed bash package

--- scripts/update-request-mapping-template.sh
+++ scripts/update-request-mapping-template.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-# Read the list of queries from the file
-QUERY_LIST=$(cat queries.txt)
+# Data Source Name
+DATA_SOURCE_NAME="LambdaDataSource"
+
+# Fetch queries from Appsyncupdate.yaml
+QUERY_LIST=$(yq e '.queries[]' src/Appsyncupdate.yaml)
 
 # Update request mapping templates for Queries
 for query in $QUERY_LIST; do
-  if [[ -f src/Queries/$query/request_mapping_template.graphql ]]; then
-    aws appsync update-resolver \
-      --api-id $API_ID \
-      --type-name Query \
-      --field-name $query \
-      --request-mapping-template file://src/Queries/$query/request_mapping_template.graphql \
-      --response-mapping-template file://src/Queries/$query/response_mapping_template.graphql \
-      --data-source-name LambdaDataSource
-  fi
+  aws appsync update-resolver \
+    --api-id $API_ID \
+    --type-name Query \
+    --field-name $query \
+    --request-mapping-template file://src/Queries/$query/request_mapping_template.graphql \
+    --response-mapping-template file://src/Queries/$query/response_mapping_template.graphql \
+    --data-source-name $DATA_SOURCE_NAME
 done
 
-# Read the list of mutations from the file
-MUTATION_LIST=$(cat mutations.txt)
+# Fetch mutations from Appsyncupdate.yaml
+MUTATION_LIST=$(yq e '.mutations[]' src/Appsyncupdate.yaml)
 
 # Update request mapping templates for Mutations
 for mutation in $MUTATION_LIST; do
-  if [[ -f src/Mutations/$mutation/request_mapping_template.graphql ]]; then
-    aws appsync update-resolver \
-      --api-id $API_ID \
-      --type-name Mutation \
-      --field-name $mutation \
-      --request-mapping-template file://src/Mutations/$mutation/request_mapping_template.graphql \
-      --response-mapping-template file://src/Mutations/$mutation/response_mapping_template.graphql \
-      --data-source-name LambdaDataSource
-  fi
+  aws appsync update-resolver \
+    --api-id $API_ID \
+    --type-name Mutation \
+    --field-name $mutation \
+    --request-mapping-template file://src/Mutations/$mutation/request_mapping_template.graphql \
+    --response-mapping-template file://src/Mutations/$mutation/response_mapping_template.graphql \
+    --data-source-name $DATA_SOURCE_NAME
 done
--- scripts/update-response-mapping-template.sh
+++ scripts/update-response-mapping-template.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-# Read the list of queries from the file
-QUERY_LIST=$(cat queries.txt)
+# Data Source Name
+DATA_SOURCE_NAME="LambdaDataSource"
+
+# Fetch queries from Appsyncupdate.yaml
+QUERY_LIST=$(yq e '.queries[]' src/Appsyncupdate.yaml)
 
 # Update response mapping templates for Queries
 for query in $QUERY_LIST; do
-  if [[ -f src/Queries/$query/response_mapping_template.graphql ]]; then
-    aws appsync update-resolver \
-      --api-id $API_ID \
-      --type-name Query \
-      --field-name $query \
-      --response-mapping-template file://src/Queries/$query/response_mapping_template.graphql \
-      --data-source-name LambdaDataSource
-  fi
+  aws appsync update-resolver \
+    --api-id $API_ID \
+    --type-name Query \
+    --field-name $query \
+    --response-mapping-template file://src/Queries/$query/response_mapping_template.graphql \
+    --data-source-name $DATA_SOURCE_NAME
 done
 
-# Read the list of mutations from the file
-MUTATION_LIST=$(cat mutations.txt)
+# Fetch mutations from Appsyncupdate.yaml
+MUTATION_LIST=$(yq e '.mutations[]' src/Appsyncupdate.yaml)
 
 # Update response mapping templates for Mutations
 for mutation in $MUTATION_LIST; do
-  if [[ -f src/Mutations/$mutation/response_mapping_template.graphql ]]; then
-    aws appsync update-resolver \
-      --api-id $API_ID \
-      --type-name Mutation \
-      --field-name $mutation \
-      --response-mapping-template file://src/Mutations/$mutation/response_mapping_template.graphql \
-      --data-source-name LambdaDataSource
-  fi
+  aws appsync update-resolver \
+    --api-id $API_ID \
+    --type-name Mutation \
+    --field-name $mutation \
+    --response-mapping-template file://src/Mutations/$mutation/response_mapping_template.graphql \
+    --data-source-name $DATA_SOURCE_NAME
 done
