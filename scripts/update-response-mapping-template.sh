050fd74d6a029c6a969183bb49ab642d4c35235c
MrinalBhoumick <mrinalbhoumick0610@gmail.com>
2024-07-30 16:02:20 +0530

    Added Authentication Logic

--- scripts/update-response-mapping-template.sh
+++ scripts/update-response-mapping-template.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Fetch the list of queries and mutations from the Appsyncupdate.yaml file
-QUERY_LIST=$(yq e '.queries' src/Appsyncupdate.yml)
-MUTATION_LIST=$(yq e '.mutations' src/Appsyncupdate.yml)
+QUERY_LIST=$(yq e '.queries[]' src/Appsyncupdate.yml)
+MUTATION_LIST=$(yq e '.mutations[]' src/Appsyncupdate.yml)
 
 # Common variables
 DATA_SOURCE_NAME="LambdaDataSource"
-LAMBDA_AUTHORIZER_CONFIG="authorizerUri=arn:aws:lambda:ap-south-1:992382729083:function:appsync-lambda-authorizer"
+
+# Update GraphQL API with AWS Lambda authorization
+aws appsync update-graphql-api \
+  --api-id $API_ID \
+  --name $API_NAME \
+  --authentication-type AWS_LAMBDA \
+  --lambda-authorizer-config authorizerUri="$LAMBDA_AUTHORIZER_ARN"
 
 # Update response mapping templates for Queries
 for query in $QUERY_LIST; do
@@ -15,8 +21,7 @@ for query in $QUERY_LIST; do
     --type-name Query \
     --field-name $query \
     --data-source-name $DATA_SOURCE_NAME \
-    --response-mapping-template file://src/Queries/$query/response_mapping_template.graphql \
-    --authorization-config lambdaAuthorizerConfig={$LAMBDA_AUTHORIZER_CONFIG}
+    --response-mapping-template file://src/Queries/$query/response_mapping_template.graphql
 done
 
 # Update response mapping templates for Mutations
@@ -26,6 +31,5 @@ for mutation in $MUTATION_LIST; do
     --type-name Mutation \
     --field-name $mutation \
     --data-source-name $DATA_SOURCE_NAME \
-    --response-mapping-template file://src/Mutations/$mutation/response_mapping_template.graphql \
-    --authorization-config lambdaAuthorizerConfig={$LAMBDA_AUTHORIZER_CONFIG}
+    --response-mapping-template file://src/Mutations/$mutation/response_mapping_template.graphql
 done
